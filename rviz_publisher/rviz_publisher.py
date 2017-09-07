275c823b439007c942af3d37198690b73f5adcb0
ipa-flg-ma <marcel.albus@ipa.fraunhofer.de>
2017-09-07 11:12:28 +0200

    [CODE] add argument parser for console commands

--- rviz_publisher/rviz_publisher.py
+++ rviz_publisher/rviz_publisher.py
@@ -6,15 +6,17 @@ Created on Sep 6, 2017
 @author: flg-ma
 @attention: Auto Position Publisher for RVIZ
 @contact: marcel.albus@ipa.fraunhofer.de (Marcel Albus)
-@version: 1.0.0
+@version: 1.1.0
 """
 
 import rospy
+import argparse
 from geneus.generate import msg_type
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PointStamped
+from bcolors import TerminalColors as tc
 from std_msgs.msg import Header
 import tf
 
@@ -29,13 +31,23 @@ class RvizPublisher():
         for topic, msg_type in self.topics.iteritems():
             self.publisher[topic] = rospy.Publisher(topic, msg_type, queue_size=2)
 
+        # setup argument parser
+        self.args = self.build_parser().parse_args()
         # self.publisher_initialpose = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=2)
         # self.publisher_goal = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=2)
         # self.publisher_clicked_point = rospy.Publisher('/clicked_point', PointStamped, queue_size=2)
 
         rospy.init_node('rviz_publisher', anonymous=True)
-        # python bug... sleep NEEDED! Min: 0.5 sec
+        # python bug... sleep NEEDED!(ros tired...) Min: 0.5 sec
         rospy.sleep(.5)
+
+    def build_parser(self):
+        parser = argparse.ArgumentParser(
+            description='Publish \'/initialpose\' for RVIZ to auto-locate robot')
+        # group = parser.add_mutually_exclusive_group()
+        parser.add_argument('-g', '--goal', help='goal for robot as:ls x y R P Y', nargs='+', type=float)
+        parser.add_argument('launch', help='launch-file to read \'/initialpose\' from', type=str)
+        return parser
 
     def euler2quaternion(self, roll, pitch, yaw):
         # converts euler to quaternion
@@ -53,12 +65,12 @@ class RvizPublisher():
         roll = args[0]
         pitch = args[1]
         yaw = args[2]
+        # convert euler-angles to quaternions
         quaternion = self.euler2quaternion(roll, pitch, yaw)
 
         if msg_type is PoseWithCovarianceStamped:
             msg.pose.pose.position.x = position_x
             msg.pose.pose.position.y = position_y
-            print msg.pose.pose.position.y
             msg.pose.pose.orientation.x = quaternion[0]
             msg.pose.pose.orientation.y = quaternion[1]
             msg.pose.pose.orientation.z = quaternion[2]
@@ -84,29 +96,21 @@ class RvizPublisher():
         # generate message to publish
         msg = self.setupMessage(self.topics[topic], 'map', pose_x, pose_y, *args)
         print msg
-        # if topic is '/initialpose':
-        #     self.publisher_initialpose.publish(msg)
-        # elif topic is '/move_base_simple/goal':
-        #     self.publisher_goal.publish(msg)
-        # elif topic is '/clicked_point':
-        #     self.publisher_clicked_point.publish(msg)
-
         self.publisher[topic].publish(msg)
 
     def getParams(self):
-        filename = '/home/flg-ma/git/catkin_ws/src/msh/msh_bringup/launch/t_passage.launch'
+        filename = self.args.launch
         with open(filename, 'r') as f:
             content = f.readlines()
 
         position = {}
         for line in content:
             if 'initial_config' in line and 'default' in line:
-                print line
-                print line[line.index('-x'):line.index('"/>')]
-                position['x'] = line[line.index('-x') + 3:line.index('-y')-1]
-                position['y'] = line[line.index('-y') + 3:line.index('-R')-1]
-                position['R'] = line[line.index('-R') + 3:line.index('-P')-1]
-                position['P'] = line[line.index('-P') + 3:line.index('-Y')-1]
+                # print line
+                position['x'] = line[line.index('-x') + 3:line.index('-y') - 1]
+                position['y'] = line[line.index('-y') + 3:line.index('-R') - 1]
+                position['R'] = line[line.index('-R') + 3:line.index('-P') - 1]
+                position['P'] = line[line.index('-P') + 3:line.index('-Y') - 1]
                 position['Y'] = line[line.index('-Y') + 3:line.index('"/>')]
                 # convert str to float
                 for pos in position:
@@ -115,10 +119,22 @@ class RvizPublisher():
 
     def main(self):
         # publish initialpose as [x, y, roll, pitch, yaw]
+        output = 'RVIZ auto-position publisher with goal: [x: ' + \
+                 str(self.args.goal[0]) + '; y: ' + \
+                 str(self.args.goal[1]) + '; R: ' + \
+                 str(self.args.goal[2]) + '; P: ' + \
+                 str(self.args.goal[3]) + '; Y: ' + \
+                 str(self.args.goal[4]) + ']'
+        print tc.OKBLUE + '=' * output.__len__()
+        print output
+        print '=' * output.__len__() + tc.ENDC
         position = rp.getParams()
         rp.publish('/initialpose', position['x'], position['y'], position['R'], position['P'], position['Y'])
+        print tc.OKBLUE + '='*80 + tc.ENDC
         rospy.sleep(5)
-        rp.publish('/move_base_simple/goal', position['x'], position['y'], position['R'], position['P'], position['Y'])
+        rp.publish('/move_base_simple/goal', self.args.goal[0], self.args.goal[1], self.args.goal[2], self.args.goal[3],
+                   self.args.goal[4])
+        print tc.OKBLUE + '='*80 + tc.ENDC
 
 
 if __name__ == '__main__':
@@ -127,5 +143,3 @@ if __name__ == '__main__':
         rp.main()
     except rospy.ROSInterruptException:
         pass
-
-# TODO: add argument parser for commandline --> main.py
